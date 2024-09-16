8d11a436771df56057a81561723b9707c342a1cb
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-09-16 18:22:36 +0530

    Create project and add Dependancis

---
+++ src/main/java/com/Smart_Student_Attendance_Backend/SmartStudentAttendanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Smart_Student_Attendance_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStudentAttendanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStudentAttendanceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Smart_Student_Attendance_Backend/SmartStudentAttendanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smart_Student_Attendance_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStudentAttendanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
